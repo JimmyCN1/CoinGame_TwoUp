f6473f4c2fa7a048f275148e6618a54e4268a020
JimmyCN1 <chibueze.e.njoku@gmail.com>
2019-07-26 15:57:16 +1000

    initial implementation of CoinPairImpl

--- src/model/CoinPairImpl.java
+++ src/model/CoinPairImpl.java
@@ -3,19 +3,36 @@ package model;
 import model.interfaces.Coin;
 import model.interfaces.CoinPair;
 
-public class CoinPairImpl implements CoinPair {
+public final class CoinPairImpl implements CoinPair {
+  private Coin[] coinPair = new Coin[2];
+  
+  public CoinPairImpl() {
+    this.coinPair[0] = new CoinImpl(1);
+    this.coinPair[1] = new CoinImpl(2);
+  }
+  
   @Override
   public Coin getCoin1() {
-    return null;
+    return this.coinPair[0];
   }
   
   @Override
   public Coin getCoin2() {
-    return null;
+    return this.coinPair[1];
+  }
+  
+  @Override
+  public boolean equals(Object coinPair) {
+    return equals(coinPair);
   }
   
   @Override
   public boolean equals(CoinPair coinPair) {
-    return false;
+    return this.coinPair[0].equals(coinPair.getCoin1()) && this.coinPair[1].equals(coinPair.getCoin2());
+  }
+  
+  @Override
+  public String toString() {
+    return String.format("%s, %s", this.coinPair[0].toString(), this.coinPair[1].toString());
   }
 }
