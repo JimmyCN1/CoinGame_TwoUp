bec08f85b038d5959e06519ef7600f15ef90ed25
JimmyCN1 <chibueze.e.njoku@gmail.com>
2019-07-27 00:35:50 +1000

    impl. 90% of GameEngineCallback toString methods

--- src/view/GameEngineCallbackImpl.java
+++ src/view/GameEngineCallbackImpl.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,34 +19,59 @@ import view.interfaces.GameEngineCallback;
  */
 public class GameEngineCallbackImpl implements GameEngineCallback {
   private static final Logger logger = Logger.getLogger(GameEngineCallback.class.getName());
+  private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("L dd, yyyy HH:mm:ss");
+  private LocalDateTime dateTime;
+  private String dateTimeString;
   
   public GameEngineCallbackImpl() {
     // NOTE need to also set the console to FINE in %JRE_HOME%\lib\logging.properties
     logger.setLevel(Level.FINE);
+    this.dateTime = LocalDateTime.now();
+    this.dateTimeString = dateTime.format(dateTimeFormatter);
   }
   
   @Override
   public void playerCoinUpdate(Player player, Coin coin, GameEngine engine) {
+    //TODO: alternate implementation in timeClassMethodToStringMethod
+//    String nameOfThisMethod = getClass().getEnclosingMethod().getName();
     // intermediate results logged at Level.FINE
-    logger.log(Level.FINE, "Intermediate data to log .. String.format() is good here!");
+    logger.log(Level.FINE, String.format("%sFINE: %s %s\n",
+            timeClassMethodToString(), player.getPlayerName(), coinFlipToString(coin)));
     // TODO: complete this method to log intermediate results
   }
   
   @Override
   public void spinnerCoinUpdate(Coin coin, GameEngine engine) {
-  
+    logger.log(Level.FINE, String.format("%sFINE: Spinner %s\n",
+            timeClassMethodToString(), coinFlipToString(coin)));
   }
   
   @Override
   public void playerResult(Player player, CoinPair coinPair, GameEngine engine) {
     // final results logged at Level.INFO
-    logger.log(Level.INFO, "Result data to log .. String.format() is good here!");
+    logger.log(Level.INFO, String.format("%sINFO: %s, final result=%s, %s",
+            timeClassMethodToString(), player.getPlayerName(),
+            coinPair.getCoin1().toString(), coinPair.getCoin2().toString()));
     // TODO: complete this method to log results
   }
   
   @Override
   public void spinnerResult(CoinPair coinPair, GameEngine engine) {
+    logger.log(Level.INFO, String.format("%sINFO: Spinner, final result=%s, %s",
+            timeClassMethodToString(), coinPair.getCoin1().toString(), coinPair.getCoin2().toString()));
+    // TODO: complete this method to log intermediate results
+  }
   
+  private String timeClassMethodToString() {
+    // get name of caller method
+    StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+    String callerMethod = stackTraceElements[1].getMethodName();
+    return String.format("%s %s\n%s\n",
+            this.dateTimeString, getClass().getName(), callerMethod);
+  }
+  
+  private String coinFlipToString(Coin coin) {
+    return String.format("coin %s flipped to %s", coin.getNumber(), coin.getFace());
   }
   
   // TODO: implement rest of interface
